9ff203b0b55367adb46312732c3c44391d40972d
Abdullah <pal95@mail.ru>
2023-05-19 21:06:24 +0300

    password

---
+++ password.js
@@ -0,0 +1,34 @@
+function generatePassword(length, levelHardPass, count = 1) {
+    const alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    let passwords = [];
+    const digits = '0123456789';
+    const symbols = '!#$%&()*+,-./:;<=>?@[]^_{|}~';
+  
+    for (let j = 0; j < count; j++) {
+      let newPassword = '';
+  
+      for (let i = 0; i < length; i++) {
+        if (levelHardPass === 'easy') {
+          const randomIndex = Math.floor(Math.random() * alphabet.length);
+          const randomAlphabet = alphabet[randomIndex];
+          newPassword += randomAlphabet;
+        } else if (levelHardPass === 'medium') {
+          const randomIndex = Math.floor(Math.random() * ${alphabet} ${digits}.length);
+          const randomAlphabet = ${alphabet} ${digits}[randomIndex];
+          newPassword += randomAlphabet;
+        } else if (levelHardPass === 'hard') {
+          const randomIndex = Math.floor(Math.random() * ${alphabet} ${digits} ${symbols}.length);
+          const randomAlphabet = ${alphabet} ${digits} ${symbols}[randomIndex];
+          newPassword += randomAlphabet;
+        }
+      }
+  
+      passwords.push(newPassword);
+    }
+  
+    if (count === 1) {
+      return passwords[0];
+    }
+  
+    return passwords;
+  }
